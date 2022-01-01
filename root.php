475624b29f2553b2f8ff4d0d51172b72cd3bfaf8
farooque <786.farooque@gmail.com>
2022-01-01 15:37:16 +0530

    root.php

--- root.php
+++ root.php
@@ -2,5 +2,7 @@
 $a = "<h1>Mohammad";
 $a .= " Farooque";
 $a .= " Abdul Razzak</h1>";
+$address = "<br> Bismillah apartment Flat no. 202 A-wing Sector-12E khairnagaon koperkhairane navi mumbai";
 echo $a;
+echo $address;
 ?>
